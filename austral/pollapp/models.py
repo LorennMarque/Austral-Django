533f3a3ec66f160b19230871112d311326b2b71e
lorenzoMarquesini <lorenzomarquesini@gmail.com>
2024-05-15 11:23:57 -0300

    updaet

--- austral/pollapp/models.py
+++ austral/pollapp/models.py
@@ -5,8 +5,18 @@ class Question(models.Model):
   question_text = models.CharField(max_length=255, default="Placeholder")
   date = models.DateTimeField()
 
+  def __str__(self):
+    return f"Id: {self.id} | {self.question_text}"  # O la representación que desees
+
+
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=255)
     likes = models.IntegerField(default=0)
     date = models.DateTimeField()
+
+    def __str__(self):
+      return f"Id: {self.id} | {self.answer_text}"  # O la representación que desees
+
+    def is_trending(self):
+        return self.likes >= 3
--- austral/pollapp/views.py
+++ austral/pollapp/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from pollapp.models import Question
+
 def index(request):
-    # return HttpResponse("Hello world!")
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
-# Create your views here.
+    questions = Question.objects.order_by('-date') # menor a mayor default. Incluir el - para invertir
+
+    context = {
+        "questions":questions
+    }
+
+    return render(request, "index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse(f"This is the question number {question_id}")
